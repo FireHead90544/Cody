0caef6ecf551e977c23cd896d6a23b8617d1c145
FireHead90544 <rudranshjoshi1806@gmail.com>
2024-07-04 19:07:38 +0530

    feat: create QuoteContainer component

---
+++ React/Checkpoint/src/components/QuoteContainer.jsx
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react";
+import Quote from './Quote';
+
+const QuoteContainer = () => {
+    const [quote, setQuote] = useState({});
+    const fetchQuote = async () => {
+        try {
+            const response = await fetch('https://programming-quotesapi.vercel.app/api/random');
+            if (response.ok) {
+                const data = await response.json();
+                setQuote({ text: data.quote, author: data.author });
+            } else {
+                console.error('Error fetching quote:', response.status);
+            }
+        } catch (error) {
+            console.error('Error fetching quote:', error);
+        }
+    };
+
+    useEffect(() => {
+        fetchQuote();
+    }, []);
+
+    const handleKeyDown = (event) => {
+        console.log("Hi there key pressed");
+        if (event.key === "Space" || event.key === " " || event.key === "Enter"){
+            fetchQuote();
+        }
+    }
+
+    return (
+        <div className="h-full flex justify-center items-center" onKeyDown={handleKeyDown} tabIndex={0}>
+            {quote.text && quote.author ? <Quote text={quote.text} author={quote.author} /> : <Quote text="Loading..." author="Loading..." />}
+        </div>
+	);
+}
+
+export default QuoteContainer
